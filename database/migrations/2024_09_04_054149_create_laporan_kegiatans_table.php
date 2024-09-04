03b99183ff6c1153e119fe57d443e728bcd2b90d
Riky yahdin <rikyyahdin@gmail.com>
2024-09-04 12:44:56 +0700

    fix: all tabel BMC

--- database/migrations/2024_09_04_054149_create_laporan_kegiatans_table.php
+++ database/migrations/2024_09_04_054149_create_laporan_kegiatans_table.php
@@ -11,12 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('zakat_infaqs', function (Blueprint $table) {
+        Schema::create('laporan_kegiatans', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('judul');
-            $table->string('gambar_qris', 2048)->nullable();
             $table->text('deskripsi');
-            $table->text('isi');
+            $table->string('isi');
+            $table->string('gambar', 2048)->nullable();
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('zakat_infaqs');
+        Schema::dropIfExists('laporan_kegiatans');
     }
 };
